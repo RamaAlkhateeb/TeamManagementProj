d7e98572b666a3f0928797987a21cb984fbfddd7
RamaAlkhateeb <ramaalkhateeb26@gmail.com>
2025-07-09 11:21:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import LoginPage from './pages/Login';
 import Dashboard from "./pages/Dashboard";
 import Sidebar from './components/Sidebar';
-import UserProfilePage from './UserProfilePage';
 
 function App() {
   return (
@@ -11,7 +10,8 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/Dash" element={<div><Sidebar /> <UserProfilePage /></div>} />
+          <Route path="/Dash" element={<Dashboard/>} />
+          <Route path="/projects" element={<div><Sidebar /></div>} />
         </Routes>
     </Router>
   );
